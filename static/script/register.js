e0fd0f57d7044d477713d322ec8028b6ddf03f48
Kolia Anoh <kolia-eugene-marcel.anoh@etudiant.univ-rennes.fr>
2024-03-01 00:12:12 +0100

    Mise à jour du formulaire d'enregistrement

--- static/script/register.js
+++ static/script/register.js
@@ -3,6 +3,7 @@ const nom = document.getElementById('nom')
 const prenom = document.getElementById('prenom')
 const email = document.getElementById('email')
 const mdpr = document.getElementById('mdpr')
+const cmdpr = document.getElementById('cmdpr')
 
 formRegister.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -16,6 +17,7 @@ function checkInputs() {
   const prenomValue = prenom.value.trim();
   const emailValue = email.value.trim();
   const mdprValue = mdpr.value.trim();
+  const cmdprValue = cmdpr.value.trim();
 
   if (nomValue === '') {
     //afficher l'erreur/ajout de la class error
@@ -47,6 +49,17 @@ function checkInputs() {
   } else {
     //ajout de la class success
     setSuccessFor(mdpr);
+  }
+
+  if (cmdprValue === '') {
+    //afficher l'erreur/ajout de la class error
+    setErrorFor(cmdpr, 'Champ incorrect')
+  } else if (mdprValue !== cmdprValue) {
+    setErrorFor(cmdpr, 'Le mot de passe ne correspond pas !')
+  }
+  else {
+    //ajout de la class success
+    setSuccessFor(cmdpr);
   }
 }
 
